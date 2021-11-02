9473835778c42ef80750882e5cf332f83f805c6b
Alexniokiz <alexniokiz@gmail.com>
2021-11-02 18:04:25 +0100

    open pages

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://lespepitestech.com/startup-de-la-french-tech/arlequin-finance');
-  await browser.close();
+  const page1 = await browser.newPage();                // open new tab
+  await page1.goto('https://fr.fakenamegenerator.com');               // go to google.com       
+  await page1.click('#details > div.content > div.info > div > div.extra > dl:nth-child(12) > dd > div > a')
+  // await page.bringToFront();
 })();
